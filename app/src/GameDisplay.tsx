1b0d402c03f05b5010a2d79452760ffb2bd77945
sylvestredavid <david.sylvestre.24@gmail.com>
2026-04-28 15:24:01 +0200

    suppression zoom

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,19 +1,12 @@
 import { css } from '@emotion/react'
-import { DevToolsHub, GameTable, GameTableNavigation } from '@gamepark/react-game'
+import { DevToolsHub, GameTable } from '@gamepark/react-game'
 import { PlayerPanels } from './panels/PlayerPanels'
 
 export function GameDisplay() {
   const margin = { top: 7, left: 0, right: 0, bottom: 0 }
-  const getNavigationCss = () => {
-    return css`
-      left: 31em;
-      top: 8em;
-    `
-  }
   return (
     <>
-      <GameTable xMin={-45} xMax={44} yMin={-20} yMax={20} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
-        <GameTableNavigation css={getNavigationCss()} />
+      <GameTable xMin={-45} xMax={44} yMin={-20} yMax={20} margin={margin} zoom={false} css={process.env.NODE_ENV === 'development' && tableBorder}>
         <PlayerPanels />
         {process.env.NODE_ENV === 'development' && <DevToolsHub fabBottom="calc(1em + 6em * 1.7)" />}
       </GameTable>
