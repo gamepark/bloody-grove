67ce4510e5326f36bef7df43ada77d3f1720aedc
Romain Fromi <romain.fromi@gmail.com>
2026-05-07 12:02:47 +0200

    Minor improvements

--- app/src/locators/ArcaneOnSpiritCardLayoutLocator.ts
+++ app/src/locators/ArcaneOnSpiritCardLayoutLocator.ts
@@ -2,22 +2,17 @@
 import { MaterialType } from '@gamepark/bloody-grove/material/MaterialType.ts'
 import { DropAreaDescription, ListLocator } from '@gamepark/react-game'
 import { Location, XYCoordinates } from '@gamepark/rules-api'
+import { spiritCardDescription } from '../material/SpiritCardDescription.tsx'
 
 class ArcaneOnSpiritCardLayoutLocator extends ListLocator {
   parentItemType = MaterialType.SpiritCard
-  positionOnParent = { x: 50, y: 50 }
   gap = { x: 1.6 }
 
-  getPositionOnParent(location: Location<number, number>): XYCoordinates {
+  getPositionOnParent(location: Location): XYCoordinates {
     return location.x === undefined ? { x: 50, y: 50 } : { x: 20, y: 20 }
   }
 
-  locationDescription = new ArcaneOnSpiritCardLayoutLocatorDescription()
-}
-
-export class ArcaneOnSpiritCardLayoutLocatorDescription extends DropAreaDescription {
-  width = 6.3
-  height = 8.8
+  locationDescription = new DropAreaDescription(spiritCardDescription)
 }
 
 export const arcaneOnSpiritCardLayoutLocator = new ArcaneOnSpiritCardLayoutLocator()
