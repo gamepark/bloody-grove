5d24fa5d37b66992cd260c3b30f14c0e1475680a
sylvestredavid <david.sylvestre.24@gmail.com>
2026-07-10 22:04:23 +0200

    pas de bonus pour les cartes posées grace aux esprits venerables

--- rules/src/rules/endOfRound/elderEffect/ElderEffectPlaceCardUnderDeckInGroveRule.ts
+++ rules/src/rules/endOfRound/elderEffect/ElderEffectPlaceCardUnderDeckInGroveRule.ts
@@ -1,10 +1,8 @@
 import { isMoveItem, ItemMove, MaterialMove, PlayerTurnRule } from '@gamepark/rules-api'
 import { LocationType } from '../../../material/LocationType'
 import { MaterialType } from '../../../material/MaterialType'
-import { SpiritCard, spiritCardData, SpiritCardId } from '../../../material/SpiritCard'
 import { Memory } from '../../Memory'
 import { NextRuleHelper } from '../../helper/NextRuleHelper'
-import { RuleId } from '../../RuleId'
 
 export class ElderEffectPlaceCardUnderDeckInGroveRule extends PlayerTurnRule {
   onRuleStart(): MaterialMove[] {
@@ -18,20 +16,10 @@ export class ElderEffectPlaceCardUnderDeckInGroveRule extends PlayerTurnRule {
   }
 
   afterItemMove(move: ItemMove): MaterialMove[] {
-    const moves: MaterialMove[] = []
     if (isMoveItem(move) && move.location.type === LocationType.PlayerSpiritUnderGroveLayout) {
-      const card = this.material(MaterialType.SpiritCard).getItem(move.itemIndex)
-      const data = spiritCardData[(card.id as SpiritCardId).front as SpiritCard]
-      const additionalRules: RuleId[] = []
-      for (let i = 0; i < data.ruses; i++) additionalRules.push(RuleId.MoveSpirit)
-      for (let i = 0; i < data.arcanes; i++) additionalRules.push(RuleId.PlaceArcane)
-      if (additionalRules.length > 0) {
-        const currentNextRules: RuleId[] = this.remind(Memory.NextRules) ?? []
-        this.memorize(Memory.NextRules, [...additionalRules, ...currentNextRules])
-      }
-      moves.push(...new NextRuleHelper(this.game).nextRule())
+      return new NextRuleHelper(this.game).nextRule()
     }
-    return moves
+    return []
   }
 
   get cardOnBottomOfDeck() {
