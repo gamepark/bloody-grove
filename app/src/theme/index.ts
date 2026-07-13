4253952cb977a3d0b5253df8d9893af4bffafdb9
Romain Fromi <romain.fromi@gmail.com>
2026-07-13 14:06:08 +0200

    Cover & background

--- app/src/theme/index.ts
+++ app/src/theme/index.ts
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react'
 import { defaultTheme, GameTheme } from '@gamepark/react-game'
+import background from '../images/background.jpg'
 import { colors } from './colors'
 import { fontBody, fontDisplay } from './typography'
 
@@ -171,7 +172,11 @@ export const theme: GameTheme = {
   ...defaultTheme,
   root: {
     ...defaultTheme.root,
-    fontFamily: fontBody
+    fontFamily: fontBody,
+    background: {
+      image: background,
+      overlay: 'rgba(0, 0, 0, 0.7)'
+    }
   },
   palette: {
     primary: colors.blood,
